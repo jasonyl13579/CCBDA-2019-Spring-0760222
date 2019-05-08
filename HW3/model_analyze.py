fa22be576c4e1e74afdab6dbc17de465ac93e0f4
jasonyl13579 <43613778+jasonyl13579@users.noreply.github.com>
2019-05-08 11:29:08 +0800

    HW2 add model and example

--- HW3/model_analyze.py
+++ HW3/model_analyze.py
@@ -14,11 +14,12 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import matplotlib.pyplot as plt
 from keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
-scl = MinMaxScaler()
+
 adj_div = 0
 adj_mean = 0
 class_num = 3
+model = load_model('model/model_10' + '.h5') 
+input_days = 60 
 def oneHot(y, num_class=3):
     y_out = np.zeros(num_class)
     y_out[y] = 1
@@ -44,17 +45,20 @@ def normalize(train):
     print (adj_div)
     train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x) + 0.001))
     return train_norm
-def normalizeAndBuildTest(train, day=30):
+def normalizeAndBuildTest(train, input_days=30, predict_days=2):
     global adj_mean, adj_div
+    
     train = train.drop(["Date"], axis=1)
-    x_test = train.iloc[2:62]
-    #train_norm = scl.fit_transform(train)
-    adj_mean = np.mean(x_test)["Close"]
-    adj_div = (np.max(x_test) - np.min(x_test))["Close"]
-    train_norm = x_test.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x) + 0.001))
+    adj_mean = np.mean(train)["Close"]
+    adj_div = (np.max(train) - np.min(train))["Close"]
+    train = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x) + 0.001))
     X_test = []
-    X_test.append(np.array(train_norm))
-    return np.array(X_test)
+    for i in range(predict_days):
+        x_test = train.iloc[train.shape[0] - input_days - i: train.shape[0] - i]   
+       # train_norm = x_test.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x) + 0.001))
+        X_test.append(np.array(x_test))
+    Y_true = train.iloc[train.shape[0]-1:train.shape[0]]["Close"]
+    return np.array(X_test), np.array(Y_true)
 def buildTrain(train, pastDay=30, futureDay=5):
     X_train, Y_train, test = [], [], []
     for i in range(train.shape[0]-futureDay-pastDay):
@@ -71,46 +75,68 @@ def buildTrain(train, pastDay=30, futureDay=5):
     return np.array(X_train), np.array(Y_train), np.array(test)
 def inverse_scaler(x, mean, div):
     return x * div + mean
-model = load_model('model/model_10' + '.h5') 
+
 #model.reset_states()
 #%% multi-predict
 
-source = ['INTC', 'AMD', 'CSCO','AAPL','MU','NVDA','QCOM','AMZN','NFLX','FB','GOOG','BABA','EBAY','XLNX','MSFT','SNPS']
+#source = ['SNPS']
+source = ['INTC', 'AMD', 'CSCO','AAPL','MU','NVDA','QCOM','AMZN','NFLX','FB','GOOG','BABA','EBAY','IBM','XLNX','TXN','NOK','TSLA','MSFT','SNPS']
 predict = []
+date = '0508'
 for i in range (len(source)):
-    test = readTrain("data/0506/" + source[i] + ".csv")
+    test = readTrain("data/" + date + "/" + source[i] + ".csv")
     test_Aug = augFeatures(test)
-    test = normalizeAndBuildTest(test_Aug, day=60)
+    test, Y_true = normalizeAndBuildTest(test_Aug, input_days, 2)
     Y_predict = np.squeeze(inverse_scaler(model.predict(test),adj_mean, adj_div))
-    Y_last = inverse_scaler(test[0,59	,3],adj_mean, adj_div)
-    print (Y_predict, Y_last)
-    print((Y_predict - Y_last) / Y_last)
-    if (((Y_predict - Y_last) / Y_last *100) >= 2): 
+    Y_last = inverse_scaler(test[:,input_days-1	,3],adj_mean, adj_div)
+    y_t = inverse_scaler(Y_true ,adj_mean, adj_div)
+    # display unknown prediction
+    print (Y_predict[0], Y_last[0])
+    print((Y_predict[0] - Y_last[0]) / Y_last[0])
+    if (((Y_predict[0] - Y_last[0]) / Y_last[0] *100) >= 2): 
         print (source[i] + ": 0")
         predict.append(0)
-    elif(((Y_predict - Y_last) / Y_last *100) >= -2):
+    elif(((Y_predict[0] - Y_last[0]) / Y_last[0] *100) >= -2):
         print (source[i] + ": 1")
         predict.append(1)
     else:
         print (source[i] + ": 2")
         predict.append(2)
- 
+    
+    if (((y_t[0] - Y_last[1]) / Y_last[1] *100) >= 2): 
+        print ("Yesterday_True" + ": 0")
+    elif(((y_t[0] - Y_last[1]) / Y_last[1] *100) >= -2):
+        print ("Yesterday_True" + ": 1")
+    else:
+        print ("Yesterday_True" + ": 2")
+   # print ("Yesterday_True:" + str(Y_true[0]))
+    if (((Y_predict[1] - Y_last[1]) / Y_last[1] *100) >= 2): 
+        print ("Yesterday_predict" + ": 0")
+    elif(((Y_predict[1] - Y_last[1]) / Y_last[1] *100) >= -2):
+        print ("Yesterday_predict" + ": 1")
+    else:
+        print ("Yesterday_predict" + ": 2")
+    
+with open( 'data/' + date + '/' + date + '_0760222.txt', 'w+') as f:
+    for x in predict:
+        f.write(str(x)+ '\n')
+
 #%% predict one
 '''
-train = readTrain("SNPS.csv")
+train = readTrain("AMD.csv")
 
 train_Aug = augFeatures(train)
 
 train_norm = normalize(train_Aug)
 
-X_train, Y_train, test = buildTrain(train_norm, 60, 1)
+X_train, Y_train, test = buildTrain(train_norm, day, 1)
 
 
 Y_true = np.squeeze(inverse_scaler(Y_train, adj_mean, adj_div))
 Y = model.predict(X_train)
 Y_predict = np.squeeze(inverse_scaler(model.predict(X_train),adj_mean, adj_div))
 #print (model.predict(X_train[0:10]))
-Y_last = inverse_scaler(X_train[:,29,3],adj_mean, adj_div)
+Y_last = inverse_scaler(X_train[:,day-1,3],adj_mean, adj_div)
 plt.plot(Y_predict)
 plt.plot(Y_true)
 Rchange1 = np.zeros(np.shape(Y_predict))
@@ -133,7 +159,6 @@ print (Rchange1)
 print (Rchange2)
 #print(((Y_predict - Y_last) / Y_last) *100)
 #print((Y_true - Y_last) / Y_last*100)
-
 '''
 '''
 y_pre = model.predict(X_train)
